4b1963ddf6db26edf9db840a751466ae0d6c4562
Emir <dzhetybaev19emir@gmail.com>
2020-08-22 06:15:57 +0600

    Updates

---
+++ precache-manifest.42ec433dfe87347e61288b1bd095dc16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0101629ac8657e71b27c0bd69b766d",
+    "url": "/private-account/index.html"
+  },
+  {
+    "revision": "8945b8e7a3060a524411",
+    "url": "/private-account/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d71cc2c7e2cbd7187c84",
+    "url": "/private-account/static/css/main.b9546f70.chunk.css"
+  },
+  {
+    "revision": "8945b8e7a3060a524411",
+    "url": "/private-account/static/js/2.be48af49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/private-account/static/js/2.be48af49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71cc2c7e2cbd7187c84",
+    "url": "/private-account/static/js/main.24d8aaca.chunk.js"
+  },
+  {
+    "revision": "e38df70c8178fb57b909",
+    "url": "/private-account/static/js/runtime-main.d27238bb.js"
+  }
+]);
